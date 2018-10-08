e06e904e38103ad1e1f8ede091bd140060e818c2
zetc0de <zetc0de.id@gmail.com>
2018-10-08 12:58:40 +0700

    Tugas Kuliah Java

---
+++ tugas-kuliah/Pertemuan3.java
@@ -0,0 +1,29 @@
+public class Pertemuan3 {
+	public static void main(String[] args) {
+		int i = 37;
+		int j = 40;
+
+		double x = 33.20;
+		double y = 8.10;
+
+		System.out.println("Varibale values...");
+		System.out.println("	i = " + i);
+		System.out.println("	j = " + j);
+		System.out.println("	x = " + x);
+		System.out.println("	y = " + y);
+
+		System.out.println("Adding...");
+		System.out.println("	i + j = " + (i+j));
+		System.out.println("	x + y = " + (x+y));
+
+		System.out.println("Substracting...");
+		System.out.println("	i - j = " + (i-j));
+		System.out.println("	x - y = " + (x-y));
+
+		String nama = "Muhammad Habiburrohman";
+		int nim = 12181649;
+		System.out.println("===================================");
+		System.out.println("Nama : " + nama);
+		System.out.println("NIM : " + nim);
+	}
+}
