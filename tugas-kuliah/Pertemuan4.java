ada0ce913c22fd5c49c8e5d9b6a302123c5baaa2
zetc0de <zetc0de.id@gmail.com>
2018-10-11 20:51:06 +0700

    Tugas Pertemuan 4

---
+++ tugas-kuliah/Pertemuan4.java
@@ -0,0 +1,33 @@
+public class Pertemuan4 {
+	public static void main(String[] args) {
+		double phi,keliling;
+		int jari;
+		
+		phi = 3.14;
+		jari = 10;
+		keliling = 2 * phi * jari;
+		
+		System.out.println("==========================");
+		System.out.println("Menghitung Keliling Lingkaran :");	
+		System.out.println("Phi		:" + phi);	
+		System.out.println("Jari-jari	:" + jari);
+		System.out.println("==========================");
+		System.out.println("Hasil 		:" + keliling);	
+
+		System.out.println("==========================");
+		System.out.println("Menghitung Luas Belah Ketupat");
+		int dig1,dig2;
+		double luas;
+		dig1 = 10;
+		dig2 = 13;
+		luas = 0.5 * dig1 * dig2;
+		System.out.println("Diagonal 1	:"+ dig1);
+		System.out.println("Diagonal 2	:"+ dig2);
+		System.out.println("==========================");
+		System.out.println("Hasil		:"+luas);
+	}
+}
+
+
+
+		
